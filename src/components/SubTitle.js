9d4456475b718098887a09899df8343452436e53
Marie-Luce Beraudo <contact@marieluceberaudo.com>
2022-04-03 08:58:15 +0200

    modifications après export

--- src/components/SubTitle.js
+++ src/components/SubTitle.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const SubTitle = class extends React.Component {
   render() {
-    return <h2 class="bg-neutral-200 p-8 pt-20 pb-20 font-light text-neutral-600 normal-case text-2xl m-0">{this.props.titre}</h2>
+    return <h2 class="bg-neutral-200 p-8 pt-20 pb-20 font-light text-neutral-600 normal-case m-0">{this.props.titre}</h2>
   }
 }
 
