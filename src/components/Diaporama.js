bccfb855045a8896c92d5a620d28f7fef1fa904b
Marie-Luce Beraudo <contact@marieluceberaudo.com>
2022-04-06 17:09:57 +0200

    essai autre diaporama

--- src/components/Diaporama.js
+++ src/components/Diaporama.js
@@ -4,18 +4,20 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loa
 import { Carousel } from 'react-responsive-carousel'
 import { GatsbyImage } from 'gatsby-plugin-image'
 
+
+
 const Diaporama = ({ images }) => {
   if (!images) return null
   if (!Array.isArray(images)) return null
   console.log('données de mes images', images)
   return (
     <div>
-      <Carousel className="max-h-[400px]">
+      <Carousel>
         {images.map((image) => {
           return (
             <div className="bg-neutral-900">
               <GatsbyImage image={image.gatsbyImageData} className="opacity-60"/>
-                <p className="absolute text-5xl text-center text-white p-3 font-bold leading-relaxed rounded w-[80%] drop-shadow-2xl">
+                <p className="absolute top-0 left-[50%] ml-[-40%] text-5xl text-center text-white p-3 font-bold leading-relaxed rounded w-[80%] drop-shadow-2xl">
                   {image.description}
                 </p>
             </div>
